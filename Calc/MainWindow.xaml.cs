b7fbc9dbcbf42de01446f7743428cb55f772461d
vanya <vanya@VINNIPUH>
2023-01-07 12:58:38 +0200

    0.1

--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -22,199 +22,91 @@ namespace Calc
 
     public partial class MainWindow : Window
     {
-        double nu1, nu2;
-        int choise = 0;
-        string bn1 = "0", bn2 = "0", choiseSimbol;
-        bool newCalc1 = true, newCalc2 = true, isInput = false, dot1 = false, dot2 = false, result = false, firstZero = true, secondZero = true;
+        private const char DOT = ',';
+
+        string firstNumber = "0";
+        string secondNumber = "0";
+        char choiseSimbol;
+        bool result = false;
 
         public void Erese()
         {
             result = false;
-            choise = 0;
-            bn1 = "0";
-            bn2 = "0";
-            newCalc1 = true;
-            newCalc2 = true;
-            isInput = false;
-            dot1 = false;
-            dot2 = false;
+            firstNumber = "0";
+            secondNumber = "0";
             result = false;
             numbersText.Content = 0;
+            choiseSimbol = '\0';
         }
-        private void n2_Click(object sender, RoutedEventArgs e)
+        private void Number_Click(object sender, RoutedEventArgs e)
         {
+            var button = (Button)sender;
+            var number = button.Content.ToString();
             if (result)
             {
                 Erese();
             }
-            if (!isInput)
+            if (choiseSimbol == '\0')
             {
-                if (newCalc1)
+                firstNumber = firstNumber + number;
+                if (firstNumber.Contains(DOT) & number == "0")
                 {
-                    bn1 = "2";
-                    newCalc1 = false;
+                    numbersText.Content = firstNumber;
                 }
                 else
                 {
-                    bn1 = bn1 + "2";
+                    numbersText.Content = Convert.ToDouble(firstNumber).ToString();
                 }
-
-                numbersText.Content = bn1;
             }
             else
             {
-                if (newCalc2)
+                secondNumber = secondNumber + number;
+                if (firstNumber.Contains(DOT) & number == "0")
                 {
-                    bn2 = "2";
-                    newCalc2 = false;
+                    numbersText.Content = Convert.ToDouble(firstNumber).ToString() + choiseSimbol + secondNumber;
                 }
                 else
                 {
-                    bn2 = bn2 + "2";
+                    numbersText.Content = Convert.ToDouble(firstNumber).ToString() + choiseSimbol + Convert.ToDouble(secondNumber).ToString();
                 }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
             }
 
         }
 
-        private void n3_Click(object sender, RoutedEventArgs e)
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
         {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "3";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "3";
-                }
-
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "3";
-                    newCalc2 = false;
-                }
-                else
-                {
-                    bn2 = bn2 + "3";
-                }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-
+            var button = (Button)sender;
+            choiseSimbol = button.Content.ToString()[0];
+            numbersText.Content = Convert.ToDouble(firstNumber).ToString() + choiseSimbol + Convert.ToDouble(secondNumber).ToString();
         }
 
-        private void n4_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "4";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "4";
-                }
-
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "4";
-                    newCalc2 = false;
-                }
-                else
-                {
-                    bn2 = bn2 + "4";
-                }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-
-        }
-
-        private void plus_Click(object sender, RoutedEventArgs e)
-        {
-            choise = 1;
-            choiseSimbol= "+";
-            numbersText.Content = bn1 + choiseSimbol + bn2;
-            isInput = true;
-        }
-
-        private void minus_Click(object sender, RoutedEventArgs e)
-        {
-            choise= 2;
-            choiseSimbol= "-";
-            numbersText.Content = bn1 + choiseSimbol + bn2;
-            isInput = true;
-        }
-
-        private void multiply_Click(object sender, RoutedEventArgs e)
-        {
-            choise= 3;
-            choiseSimbol= "x";
-            numbersText.Content = bn1 + choiseSimbol + bn2;
-            isInput = true;
-        }
-
-        private void divide_Click(object sender, RoutedEventArgs e)
-        {
-            choise= 4;
-            choiseSimbol= "/";
-            numbersText.Content = bn1 + choiseSimbol + bn2;
-            isInput = true;
-        }
 
         private void equal_Click(object sender, RoutedEventArgs e)
         {
-            if (bn1 != null & bn2 != null & choise != 0)
+            if (choiseSimbol != '\0')
             {
-                nu1 = Convert.ToDouble(bn1);
-                nu2 = Convert.ToDouble(bn2);
-                if (choise == 1)
+
+                double nu1 = Convert.ToDouble(firstNumber);
+                double nu2 = Convert.ToDouble(secondNumber);
+                switch (choiseSimbol)
                 {
-                    numbersText.Content = nu1 + nu2;
-                }
-                if (choise == 2)
-                {
-                    numbersText.Content = nu1 - nu2;
-                }
-                if (choise == 3)
-                {
-                    numbersText.Content = nu1 * nu2;
-                }
-                if (choise == 4)
-                {
-                    if (nu2 == 0)
-                    {
-                        numbersText.Content = "Error";
-                    }
-                    else
-                    {
+                    case '+':
+                        numbersText.Content = nu1 + nu2;
+                        break;
+                    case '*':
+                        numbersText.Content = nu1 * nu2;
+                        break;
+                    case '-':
+                        numbersText.Content = nu1 - nu2;
+                        break;
+                    case '/':
                         numbersText.Content = nu1 / nu2;
-                    }
+                        break;
                 }
                 result = true;
             }
+            
+        
         }
 
         private void delete_Click(object sender, RoutedEventArgs e)
@@ -222,280 +114,30 @@ namespace Calc
             Erese();
         }
 
-        private void n5_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "5";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "5";
-                }
-
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "5";
-                    newCalc2 = false;
-                }
-                else
-                {
-                    bn2 = bn2 + "5";
-                }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-
-        }
-
-        private void n6_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "6";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "6";
-                }
-
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "6";
-                    newCalc2 = false;
-                }
-                else
-                {
-                    bn2 = bn2 + "6";
-                }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-
-        }
-
-        private void n7_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "7";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "7";
-                }
-
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "7";
-                    newCalc2 = false;
-                }
-                else
-                {
-                    bn2 = bn2 + "7";
-                }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-
-        }
-
         private void dot_Click(object sender, RoutedEventArgs e)
         {
-            if (!isInput)
+            if (choiseSimbol == '\0')
             {
-                if (dot1 == false)
+                if (!firstNumber.Contains(DOT))
                 {
-                    bn1 = bn1 + ",";
-                    numbersText.Content = bn1;
-                    dot1= true;
-                    newCalc1= false;
+                    firstNumber = Convert.ToDouble(firstNumber).ToString() + DOT;
+                    numbersText.Content = firstNumber;
                 }
             }
             else
             {
-                if (dot2 == false)
+                if (!secondNumber.Contains(DOT))
                 {
-                    bn2 = bn2 + ",";
-                    numbersText.Content = bn1 + choiseSimbol + bn2;
-                    dot2 = true;
-                    newCalc2= false;
+                    secondNumber = secondNumber + DOT;
+                    numbersText.Content = Convert.ToDouble(firstNumber).ToString() + choiseSimbol + Convert.ToDouble(secondNumber).ToString();
                 }
             }
-        }
-
-        private void n8_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "8";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "8";
-                }
-
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "8";
-                    newCalc2 = false;
-                }
-                else
-                {
-                    bn2 = bn2 + "8";
-                }
-
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-
-        }
-
-        private void n9_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-                if (!isInput)
-                {
-                    if (newCalc1)
-                {
-                    bn1 = "9";
-                    newCalc1= false;
-                }
-                else
-                {
-                    bn1 = bn1 + "9";
-                }
-                    
-                    numbersText.Content = bn1;
-                }
-                else
-                {
-                if (newCalc2)
-                {
-                    bn2 = "9";
-                    newCalc2 = false;
-                } else
-                {
-                    bn2 = bn2 + "9";
-                }
-
-                    numbersText.Content = bn1 + choiseSimbol + bn2;
-                }
-            
-        }
-
-        private void n1_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    bn1 = "1";
-                    newCalc1 = false;
-                }
-                else
-                {
-                    bn1 = bn1 + "1";
-                }
-                numbersText.Content = bn1;
-            }
-            else
-            {
-                if (newCalc2)
-                {
-                    bn2 = "1";
-                    newCalc2 = false;
-                } else
-                {
-                    bn2 = bn2 + "1";
-                }
-                numbersText.Content = bn1 + choiseSimbol + bn2;
-            }
-            
         }
         public MainWindow()
         {
             InitializeComponent();
             numbersText.Content = 0;
         }
-
-        private void n0_Click(object sender, RoutedEventArgs e)
-        {
-            if (result)
-            {
-                Erese();
-            }
-            if (!isInput)
-            {
-                if (newCalc1)
-                {
-                    numbersText.Content = bn1;
-                }
-                else
-                {
-                    numbersText.Content = bn1 + "0";
-                    bn1 = bn1 + "0";
-                }
-            }
-            else
-            {
-                if (!newCalc2)
-                {
-                    bn2 = bn2 + "0";
-                    numbersText.Content = bn1 + choiseSimbol + bn2;
-                }
-            }
-        }
-    }   
+    }
 }
 
